01d8e895d2ea1a350a27a1eb310fdaf9ab212af7
yz <274804397@qq.com>
2024-03-29 21:50:17 +0800

    添加拒绝理由

--- config/index.js
+++ config/index.js
@@ -1,15 +1,16 @@
 module.exports = {
     // 远程数据库
-    // DBHOST: 'www.hyperyz.com',
-    // DBNAME: 'testtest',
-    // DBUSER: 'testtest',
-    // DBPASSWORD: '123456',
+    DBHOST: 'www.hyperyz.com',
+    DBNAME: 'travel',
+    DBUSER: 'hyperpyz',
+    DBPASSWORD: '123456',
 
     // 本地数据库
-    DBHOST: 'localhost',
-    DBNAME: 'test',
-    DBUSER: 'root',
-    DBPASSWORD: '510524xzw',
+    // DBHOST: 'localhost',
+    // DBNAME: 'test',
+    // DBUSER: 'root',
+    // DBPASSWORD: '1234',
+    
     PORT: 3000,
     secret: '' //密钥    
 }
