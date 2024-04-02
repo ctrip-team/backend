5fa6951849edb7ef07433132fad7b841e296852e
yz <274804397@qq.com>
2024-04-02 23:05:42 +0800

    更改服务启动方式，使用dotenv区别生产开发环境

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express");
 const cors = require('cors')
+require('dotenv').config()
 const app = express();
+const http = require("http");
+const https = require("https");
+const fs = require("fs");
+const path = require("path");
 const { PORT } = require('./config')
 const userRouter = require('./router/user.js')
 const travelRouter = require('./router/travel.js')
@@ -23,8 +28,29 @@ app.use('/api/index', indexRouter)
 app.use('/api/role', roleRouter)
 app.use('/api/my', myRouter)
 
+// 创建服务
+function createServer() {
+    if (process.env.ENV === "production") {
+        const options = {
+            key: fs.readFileSync(
+                path.join(__dirname, "/public/privkey.key")
+            ),
+            cert: fs.readFileSync(
+                path.join(__dirname, "/public/fullchain.pem")
+            ),
+        };
+        return https.createServer(options, app);
+    } else {
+        return http.createServer(app);
+    }
+}
 
-app.listen(PORT, () => {
-    console.log(`Server running at http://192.168.0.102:${PORT}`);
-    console.log(`For API docs, http://192.168.0.102:${PORT}/api-docs`);
-});
+const server = createServer();
+server.listen(process.env.PORT, () => {
+    console.log(`服务已启动在${process.env.PORT}端口`);
+})
+
+// app.listen(PORT, () => {
+//     console.log(`Server running at http://192.168.0.102:${PORT}`);
+//     console.log(`For API docs, http://192.168.0.102:${PORT}/api-docs`);
+// });
