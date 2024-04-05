b8f74a2b7e00260af1679363b6d3406d8964986b
finabin <13803371+finabin@user.noreply.gitee.com>
2024-04-05 19:15:55 +0800

    数据库连接更新

--- config/index.js
+++ config/index.js
@@ -1,15 +1,15 @@
 module.exports = {
     // 远程数据库
-    DBHOST: 'www.hyperyz.com',
-    DBNAME: 'travel',
-    DBUSER: 'hyperpyz',
-    DBPASSWORD: '123456',
+    // DBHOST: 'www.hyperyz.com',
+    // DBNAME: 'travel',
+    // DBUSER: 'hyperpyz',
+    // DBPASSWORD: '123456',
 
     // 本地数据库
-    // DBHOST: 'localhost',
-    // DBNAME: 'test',
-    // DBUSER: 'root',
-    // DBPASSWORD: '1234',
+    DBHOST: 'localhost',
+    DBNAME: 'test',
+    DBUSER: 'root',
+    DBPASSWORD: '510524xzw',
 
     PORT: 3000,
     secret: 'weareno1' //密钥    
